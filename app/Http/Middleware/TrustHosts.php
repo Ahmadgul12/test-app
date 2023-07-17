370ad6406047906c4577cae0382f9e4a0960642a
ahmadgul <ag2732935@gmail.com>
2023-07-17 14:33:48 +0430

    test

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -9,12 +9,12 @@ class TrustHosts extends Middleware
     /**
      * Get the host patterns that should be trusted.
      *
-     * @return array<int, string|null>
-     */
-    public function hosts(): array
-    {
-        return [
-            $this->allSubdomainsOfApplicationUrl(),
-        ];
-    }
-}
+                        * @return array<int, string|null>
+                        */
+                        public function hosts(): array
+                        {
+                            return [
+                                $this->allSubdomainsOfApplicationUrl(),
+                            ];
+                        }
+                    }
